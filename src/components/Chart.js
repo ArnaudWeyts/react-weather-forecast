8fccf545fb88b3c8833e587d31300c8e0bc47f38
Arnaud Weyts <arnaud.weyts@gmail.com>
2017-01-15 17:53:05 +0100

    added google maps for city location

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -14,5 +14,5 @@ export default (props) => {
       </Sparklines>
       <div>{average(props.data)} {props.units}</div>
     </div>
-  )
+  );
 }
--- src/containers/WeatherList.js
+++ src/containers/WeatherList.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import Chart from '../components/Chart';
+import SimpleMap from '../components/SimpleMap';
 
 class WeatherList extends Component {
   renderWeather(cityData) {
@@ -8,9 +9,12 @@ class WeatherList extends Component {
     const pressures = cityData.list.map(values => values.main.pressure);
     const humidities = cityData.list.map(values => values.main.humidity);
 
+    const {lat, lon} = cityData.city.coord;
+    const {id} = cityData.city;
+
     return (
-      <tr key={cityData.city.id}>
-        <th scope="row">{cityData.city.name}</th>
+      <tr key={id}>
+        <td><SimpleMap lat={lat} lon={lon}/></td>
         <td><Chart data={temps} color="blue" units="K"/></td>
         <td><Chart data={pressures} color="green" units="hPa"/></td>
         <td><Chart data={humidities} color="black" units="%" /></td>
